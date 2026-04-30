8fc49be7fb174d6d7529917f555f15fa05a3a139
Amelia1 <disputedbowl@gmail.com>
2026-04-30 16:13:27 +0200

    www

---
+++ CardReader/card-creator.lua
@@ -0,0 +1,23 @@
+local manipulator = peripheral.find("magnetic_card_manipulator")
+
+local function CreateCard(sec_level)
+    
+    if manipulator.hasCard() then
+        if sec_level == "5" then
+            manipulator.setLabel("O5 KEYCARD")
+        else
+            manipulator.setLabel("L%s Keycard", sec_level)
+        end
+
+        manipulator.writeCard(sec_level)
+        
+    end
+end
+
+
+while true do
+    term.clear()
+    write("Level: ")
+    local input = read()
+    CreateCard(input)
+end
